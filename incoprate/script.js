12ab70bb569732986a9a40b8fe366bbf50f20691
vikash <kumarvikashv681@gmail.com>
2022-06-20 17:15:43 +0530

    learned incopration

---
+++ incoprate/script.js
@@ -0,0 +1,10 @@
+// to use jquery use 'jquery' or $
+
+//selecting an html element and applying css to it
+$("h1").css("color","red"); // h1 to red
+$("body").css("background-color","blue") //background to blue
+
+// run jquery when dom ready
+// $("document").ready(function(){
+
+// })
